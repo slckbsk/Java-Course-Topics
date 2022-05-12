8f13acc09a5d26b68eda3b732549e299941a4ba9
SelçukBaşak <selcukbasak5@gmail.com>
2022-05-13 02:46:42 +0300

    Update DbHelper.java

--- WorldCities/src/DbHelper.java
+++ WorldCities/src/DbHelper.java
@@ -8,7 +8,7 @@ import java.sql.*;
  */
 public class DbHelper {
     private String userName = "root";
-    private String password = "mysql1234.";
+    private String password = "********";
     private String dbUrl = "jdbc:mysql://localhost:3306/world";
     
     public Connection getConnection() throws SQLException{
